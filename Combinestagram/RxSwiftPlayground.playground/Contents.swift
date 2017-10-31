8f6a550989e7128fae15a73a2a51886c0c6d0b6b
HaiVL <hai.softech@gmail.com>
2017-10-31 17:31:03 +0700

    Chapter 7 Research flatMap

--- Combinestagram/RxSwiftPlayground.playground/Contents.swift
+++ Combinestagram/RxSwiftPlayground.playground/Contents.swift
@@ -14,14 +14,34 @@ func print<T: CustomStringConvertible>(label: String, event: Event<T>) {
     print(label, event.element ?? event.error ?? event)
 }
 
-let disposeBag = DisposeBag()
-
-example(of: "skipWhile") {
-
-   Observable.of(4, 3, 4, 5, 6)
-    .skipWhile { integer in
-        integer % 2 == 0
-    }.subscribe(onNext: {
-        print($0)
-    }).addDisposableTo(disposeBag)
+struct Student {
+    var score: Variable<Int>
 }
+
+let bag = DisposeBag()
+
+example(of: "flatMapLatest") {
+
+    let ryan = Student(score: Variable(80))
+    let charlotte = Student(score: Variable(90))
+    
+
+    let student = PublishSubject<Student>()
+
+    student.asObserver()
+        .flatMapLatest {
+            $0.score.asObservable()
+        }
+        .subscribe(onNext: {
+            print("value: \($0)")
+        }).addDisposableTo(bag)
+
+    student.onNext(ryan)
+    ryan.score.value = 85
+
+    student.onNext(charlotte)
+    ryan.score.value = 95
+    charlotte.score.value = 100
+}
+
+
